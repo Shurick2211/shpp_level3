622a2d4b0122b5103dab2173bce4ca2d0d0cf0b4
Alexandr Nimko <shurick2211@gmail.com>
2022-06-29 06:45:29 +0300

    6 ready

--- src/com/shpp/p2p/cs/onimko/assignment3/Assignment3Part6.java
+++ src/com/shpp/p2p/cs/onimko/assignment3/Assignment3Part6.java
@@ -1,6 +1,8 @@
 package com.shpp.p2p.cs.onimko.assignment3;
 
+import acm.graphics.GLabel;
 import acm.graphics.GOval;
+import acm.graphics.GRect;
 import com.shpp.cs.a.graphics.WindowProgram;
 
 import java.awt.*;
@@ -18,33 +20,78 @@ public class Assignment3Part6 extends WindowProgram {
    * getHeight(), which return the *exact* width and height of the window.
    */
   public static final int APPLICATION_WIDTH = 300;
-  public static final int APPLICATION_HEIGHT =300;
+  public static final int APPLICATION_HEIGHT = (int) (APPLICATION_WIDTH/3*2+MENU_SIZE);
+
+  /*
+   * Time of video in seconds
+   */
+  private final int TIME_VIDEO = 5;
+
+  /*
+   * Frame const
+   */
+  private final double FRAME_PER_SECOND = 24;
+  private final double FRAME_PAUSE = 1000 / FRAME_PER_SECOND;
+  private final int ALL_FRAMES = (int) (TIME_VIDEO * 1000 / FRAME_PAUSE);
+
+  /*
+   * String's consts for labels
+   */
+  private final String UP_STR = "Добрий вечір!";
+  private final String DWN_STR = "Ми з України!";
+
+
+  /*
+   * Font of name's country
+   */
+  public static final Font FONT = new Font("Bold", Font.BOLD, (int) (APPLICATION_HEIGHT-MENU_SIZE)/6);
+
 
   /**
    * It is start method, where print result
    */
   public void run() {
-
-    GOval circle = new GOval (120,25,30,30);
-    circle.setColor(Color.GREEN);
-    circle.setFillColor(Color.BLUE);
-    circle.setFilled(true);
-    add(circle);
-    println(getWidth()-30);
-    double sin;
-    for (int i = 0; i<98; i++){
-      sin = Math.sin(i);
-      pause(100);
-
-        circle.move(0 ,(100)*sin);
-
-      //println((int)circle.getY() + " : " + sin );
-
-
+    drawFlag();
+    GLabel upText = label( UP_STR, Color.YELLOW);
+    upText.setLocation(getWidth(), getHeight()/6+upText.getHeight()/2);
+    GLabel dwnText = label( DWN_STR, Color.BLUE);
+    dwnText.setLocation(-dwnText.getWidth(),getHeight()/2+getHeight()/6+upText.getHeight()/2);
+    add(upText);
+    add(dwnText);
+    double endXUP = (getWidth() - upText.getWidth())/2;
+    double dxUP = (getWidth()-endXUP)/ALL_FRAMES;
+    double endXDwn = (getWidth() - upText.getWidth())/2;
+    double dxDwn = (getWidth()-endXUP)/ALL_FRAMES;
+    for (int i = 0; i < ALL_FRAMES; i++) {
+      if (upText.getX() >= endXUP)
+      upText.setLocation(upText.getX()-dxUP,upText.getY());
+      if (dwnText.getX() <= endXDwn)
+      dwnText.setLocation(dwnText.getX()+dxDwn, dwnText.getY());
+      pause(FRAME_PAUSE);
     }
 
-
-
   }
+
+  /**
+   * Method draws flag
+   */
+  private void drawFlag() {
+    GRect upLine = new GRect(0,0, getWidth(),getHeight() /2);
+    upLine.setFilled(true);
+    upLine.setColor(Color.BLUE);
+    GRect downLine = new GRect(0,getHeight() /2, getWidth(), getHeight()/2);
+    downLine.setFilled(true);
+    downLine.setColor(Color.YELLOW);
+    add(upLine);
+    add(downLine);
+  }
+
+  private GLabel label (String text, Color color) {
+    GLabel label = new GLabel(text,getWidth(),getHeight()/6);
+    label.setColor(color);
+    label.setFont(FONT);
+    return label;
+  }
+
 
 }
